3a2e91ba0dcda0c0a7c733ba3d4e548f9398ac13
Yahya Naji <najiyahya2003@gmail.com>
2025-07-29 09:40:50 +0300

    add Read() funcion to config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,39 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
 	Db_url            string `json:"db_url"`
 	Current_user_name string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	config := Config{}
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return config, nil
+	}
+	bytes, err := os.ReadFile(configFilePath)
+	if err != nil {
+		return config, err
+	}
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDirPath, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	configFilePath := homeDirPath + "/" + configFileName
+	return configFilePath, nil
 }
