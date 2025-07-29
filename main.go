cee84e90d10e803b210d377ba51f9cd6df8ae200
Yahya Naji <najiyahya2003@gmail.com>
2025-07-29 09:56:31 +0300

    add test code to main function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/R0Xps/gatorcli/internal/config"
+)
+
+func main() {
+	conf, err := config.Read()
+	fmt.Println(conf)
+	fmt.Println(err)
+	conf.SetUser("yahya")
+	conf, err = config.Read()
+	fmt.Println(conf)
+	fmt.Println(err)
 }
